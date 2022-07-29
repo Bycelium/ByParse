48a09ee486634a6203711915d55bd7c3a41245b7
Antoine Delègue <delegue.antoine@gmail.com>
2022-07-29 18:04:42 +0200

    Big code clean up, rewrote everything in an object oriented style.

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Python Static Analysis tools
 
+Build with Python 3.8.10 in mind.
 
 A set of scripts that perform static analysis on Python code as well as some sample projects
 to test them.
+
+## Static analysis of the static analysis:
+
+Useful commands to make sure everything works properly:
+
+- `pytest to_graph.py -s`
+- `pylint to_graph.py`
+- `mypy to_graph.py`
--- to_graph.py
+++ to_graph.py
@@ -12,22 +12,16 @@ from pathlib import Path
 import os
 import networkx as nx # type: ignore
 from matplotlib import pyplot as plt # type: ignore
-from dataclasses import dataclass
-from typing import Union, Set
+from dataclasses import dataclass, field
+from typing import Union, Set, List, Dict, Optional, Any
+import random
+import string
 
+def random_string(l=12):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for _ in range(l))
 
-@dataclass
-class PythonSourceFile:
-    basename: str
-    path: str
-    package_name: Union[str, None]
-    function_names: Set[str]
-    import_paths: Set[str]
-
-psf = PythonSourceFile("","",None,set(),set())
-
-
-def package_name_to_path(package_name, filepath):
+def package_name_to_path(package_name: str, filepath: str) -> Optional[str]:
     """
         Return the path of the package named <package_name>
         that was imported from the file located at <filepath>
@@ -45,112 +39,324 @@ def package_name_to_path(package_name, filepath):
         return spec.origin
     else:
         # Try a local resolve based on filepath:
-        package_name: str = package_name.replace(".","./")
+        package_name = package_name.replace(".","./")
         package_path: Path = Path(filepath).parent / Path(package_name)
         if package_path.exists():
             if package_path.is_dir():
-                return package_path / Path("__init__.py")
+                return str(package_path / Path("__init__.py"))
             else:
-                return package_path
+                return str(package_path)
         else: 
             package_path = Path(str(package_path) + ".py")
             if package_path.exists():
-                return package_path
+                return str(package_path)
     return None # Unable to resolve the package
 
-
-
-alreadySeen = set() # infinite recursion safe-guard
-def resolveImport(n, filepath):
-    node2 = Path(filepath).name
-    p = package_name_to_path(n, str(filepath))
-
-    # print(f"Name: {n}")
-    # print(f"  Path: {p}")
-
-    if p is not None:
-        node1 = Path(p).name
-        if "site-packages" in str(p):
-            path_component = str(p).split(os.sep)
-            # The name of the module is the element after site-packages
-            module_name = path_component[path_component.index("site-packages")+1]
-            node1 = f"[{module_name}] {node1}"
-        G.add_node(node1)
-        G.add_edge(node2, node1)
-    else:
-        node1 = n
-        G.add_edge(node2, node1)
-
-    if "lib" in str(p) and "site-packages" not in str(p):
-        return # Don't resolve native dependencies to avoid noise in graph
-
-    if p is not None and str(p).endswith(".py"):
-        if p in alreadySeen: return
-        alreadySeen.add(p)
-        try:
-            s = ast.parse(source=open(p,"r",encoding="utf8").read(), filename=p)
-            exploreTree(s, p)
-        except SyntaxError:
-            pass # Ignore malformed file.
-
-def exploreTree(s, filepath):
+def test_package_name_to_path():
     """
-        Recursive ast tree explorer to extract import statements.
+    Usage example for package_name_to_path.
+    Executable using `pytest to_graph.py -s`
     """
-    fp = Path(filepath).absolute()
-    # Enumerate the types we can encounter.
-    if isinstance(s, ast.Module):
-        for i in s.body:
-            exploreTree(i,filepath)
-    elif isinstance(s, ast.If) or \
-         isinstance(s, ast.For) or \
-         isinstance(s, ast.While) or \
-         isinstance(s, ast.With) or \
-         isinstance(s, ast.ExceptHandler):
-        for i in s.body:
-            exploreTree(i,filepath)
-    elif isinstance(s, ast.FunctionDef):
-        for i in s.body:
-            exploreTree(i,filepath)
-    elif isinstance(s, ast.Try):
-        for i in s.body:
-            exploreTree(i,filepath)
-        for i in s.handlers:
-            exploreTree(i,filepath)
-        for i in s.finalbody:
-            exploreTree(i,filepath)
-    elif isinstance(s, ast.Import):
-        for name in s.names:
-            resolveImport(name.name, fp)
-    elif isinstance(s, ast.ImportFrom):
-        for name in s.names:
-            module_name = "." + name.name
-            if s.module is not None:
-                resolveImport(s.module, fp)
-                module_name = s.module + "." + name.name
+    numpy = package_name_to_path("numpy", ".")
+    assert numpy.endswith(str(Path("lib/site-packages/numpy/__init__.py")))
 
-            # Try to import the thing as a whole file, if not just the module name
-            # We need to handle both bases:
-            # from <big module> import <filename>
-            # from <small module> import <specific function>
-            resolveImport(module_name, fp)
+    utils = package_name_to_path("utils", "./toy_project/__main__.py")
+    assert utils.endswith(str(Path("utils/__init__.py")))
 
-    else:
-        pass
-        # Ignore these, they do not contain nested statements
-        #print(ast.dump(s))
-        #print("-----")
-    # print(ast.dump(s.body[5]))
+    pyplot = package_name_to_path("matplotlib.pyplot", ".")
+    assert pyplot.endswith(str(Path("lib/site-packages/matplotlib/pyplot.py")))
 
+    doesnotexist = package_name_to_path("doesnotexist", "./toy_project/__main__.py")
+    assert doesnotexist is None
+
+
+
+@dataclass
+class FileLocation:
+    """
+    Represents a specific place inside a file
+    """
+    path: str
+    lint: int
+
+@dataclass
+class UniqueIdentifier:
+    """
+    A wrapper around a string. Used to indicate that 2 strings are in the same
+    context. Used more as a type hint than an actual class.
+    """
+    v: str
+
+@dataclass
+class PythonFunctionDefinition:
+    """
+    Represents a definition of a python function
+    """
+    name: str
+    arguments: List[str]
+    path: str # The file where the function was defined
+    line: int
+    callingSites: Set[FileLocation]
+
+class PythonSourceFile:
+    """
+    Represents a piece of python code.
+    """
+    path: Optional[str] = None
+
+    # A short and displayable name for the code, like "utils.py"
+    basename: str = ""
+    
+    # A unique name that always exists. If the code is in a file, it is the path. Otherwise, it can be a random string.
+    unique_name: Union[UniqueIdentifier, str]
+ 
+    # If the file is part of a package, the name of the package is contained here
+    package_name: Optional[str] = None
+
+    # The set of functions that are defined by the piece of code
+    function_names: Set[str] = set()
+
+    # The unique_names
+    import_paths: Set[Union[UniqueIdentifier, str]] = set()
+
+    # Static field containing all PythonSourceFile instances
+    instances: Dict[str, Any] = {}
+
+    error_messages: List[str] = []
+
+    def resolveImport(self, importName: str, exhaustiveResolution = False):
+        """
+            Resolve an import based on importName.
+            Keep building the dependency tree based on it.
+            This function can create new PythonSourceFile and it thus indirectly recursive.
+            importName can look like this:
+            - numpy
+            - .utils (in case of from . import utils)
+            - math
+            - math.random
+            - matplotlib.pyplot
+            All these cases are handled here.
+        """
+        # We don't perform import resolution if the code is not inside a file.
+        if self.path is None: return
+        p = package_name_to_path(importName, self.path)
+        
+        if p is None:
+            n = importName.split(".")
+            i = len(n)
+            # Try to resolve path to packages of decreasing length.
+            while p is None and i > 0:
+                p = package_name_to_path('.'.join(n[0:i]),self.path)
+                i -= 1
+
+            n.insert(0,"")
+            i = len(n)
+            while p is None and i > 1:
+                p = package_name_to_path('.'.join(n[0:i]),self.path)
+                i -= 1
+            if p is None:
+                # Give up: we cannot resolve this. We still log it.
+                # But only if we're sure this was not already imported (there is an import with the same basename)
+                psf = None
+                for i in PythonSourceFile.instances:
+                    if PythonSourceFile.instances[i].basename == importName:
+                        psf = PythonSourceFile.instances[i]
+                        break
+                if psf is None:
+                    psf = PythonSourceFile(None, False)
+                psf.basename = importName
+                self.import_paths.add(psf.unique_name)
+                return
+
+        if not exhaustiveResolution:
+            if "lib" in p and "site-packages" not in p:
+                # Don't resolve native dependencies to avoid noise in graph
+                # We create the PythonSourceObject, with without parsing
+                psf = PythonSourceFile(p, False)
+                psf.basename = importName
+                self.import_paths.add(psf.unique_name)
+                return
+
+        psf = PythonSourceFile(p)
+        self.import_paths.add(psf.unique_name)
+
+    def exploreTree(self, s: ast.AST, namespace="", importResolution = True):
+        """
+            Recursive depth-first explorer of the abstract syntax tree.
+        """
+        if s is None: return
+        # Enumerate the types we can encounter
+        if isinstance(s, ast.Module):
+            for i in s.body:
+                self.exploreTree(i, namespace)
+        elif isinstance(s, ast.If) or \
+             isinstance(s, ast.For) or \
+             isinstance(s, ast.While) or \
+             isinstance(s, ast.With) or \
+             isinstance(s, ast.ExceptHandler):
+            for i in s.body:
+                self.exploreTree(i, namespace)
+        elif isinstance(s, ast.ClassDef):
+            # Ignore args, returns, type_comment and decorator_list
+            if namespace is not None:
+                self.function_names.add(namespace + s.name)
+            for i in s.body:
+                self.exploreTree(i, None)
+        elif isinstance(s, ast.FunctionDef):
+            if namespace is not None:
+                namespace += s.name+"."
+            for i in s.body:
+                self.exploreTree(i, namespace)
+        elif isinstance(s, ast.Try):
+            for i in s.body:
+                self.exploreTree(i, None)
+            for j in s.handlers:
+                self.exploreTree(j, None)
+            for k in s.finalbody:
+                self.exploreTree(k, None)
+        elif isinstance(s, ast.Import) and importResolution:
+            for name in s.names:
+                self.resolveImport(name.name)
+        elif isinstance(s, ast.ImportFrom) and importResolution:
+            for name in s.names:
+                if s.module is not None:
+                    module_name = s.module + "." + name.name
+                    self.resolveImport(module_name)
+                else:
+                    self.resolveImport("." + name.name)
+
+        elif isinstance(s, ast.Assign):
+            # read only the right part of the assignement
+            self.exploreTree(s.value, None)
+        elif isinstance(s, ast.AugAssign):
+            self.exploreTree(s.value, None)
+        elif isinstance(s, ast.AnnAssign):
+            self.exploreTree(s.value, None)
+        elif isinstance(s, ast.Return):
+            self.exploreTree(s.value, None)
+        elif isinstance(s, ast.Index): # stuff inside [] when indexing array
+            self.exploreTree(s.value, None)
+        elif isinstance(s, ast.FormattedValue): # stuff inside f-strings
+            self.exploreTree(s.value, None)
+        elif isinstance(s, ast.Expr):
+            self.exploreTree(s.value, None)
+        elif isinstance(s, ast.Subscript):
+            self.exploreTree(s.value, None)
+            self.exploreTree(s.slice, None)
+        elif isinstance(s, ast.BinOp): # 3 + 5
+            self.exploreTree(s.left, None)
+            self.exploreTree(s.right, None)
+        elif isinstance(s, ast.BoolOp):
+            for i in s.values:
+                self.exploreTree(i, None)
+        elif isinstance(s, ast.JoinedStr):
+            for i in s.values:
+                self.exploreTree(i, None)
+        elif isinstance(s, ast.Compare): # "s" not in stuff
+            self.exploreTree(s.left, None)
+            for i in s.comparators:
+                self.exploreTree(i, None)
+        elif isinstance(s, ast.Yield):
+            self.exploreTree(s.value, None)
+        elif isinstance(s, ast.Raise):
+            self.exploreTree(s.exc, None)
+        elif isinstance(s, ast.List) or isinstance(s, ast.Tuple):
+            for i in s.elts:
+                self.exploreTree(i, None)
+        elif isinstance(s, ast.Dict):
+            for i in s.keys:
+                self.exploreTree(i, None)
+            for i in s.values:
+                self.exploreTree(i, None)
+        elif isinstance(s, ast.Constant):
+            pass
+        elif isinstance(s, ast.Delete):
+            pass
+        elif isinstance(s, ast.Attribute):
+            pass # variable inside class (np.array)
+        elif isinstance(s, ast.Name):
+            pass # variable outside class
+        elif isinstance(s, ast.Call): # A function call!
+            for i in s.args:
+                self.exploreTree(i, None)
+        else:
+            # Ignore these, they do not contain nested statements
+            pass
+            # print(ast.dump(s))
+            # print("-----")
+
+    def to_directed_graph(self) -> nx.DiGraph:
+        G = nx.DiGraph()
+        # Simple BFS algorithm with a set to avoid infinite loops.
+        visit_stack = [self]
+        visited = set()
+
+        while len(visit_stack) > 0:
+            node = visit_stack.pop()
+            if node in visited: continue
+            visited.add(node)
+            G.add_node(node.unique_name)
+            for i in node.import_paths:
+                inst = PythonSourceFile.instances[i]
+                visit_stack.append(inst)
+                if not G.has_node(inst.unique_name):
+                    G.add_node(inst.unique_name)
+                G.add_edge(node.unique_name, inst.unique_name)
+
+        return G
+
+    def __init__(self, path, parsing=True):
+        self.path = path
+        self.import_paths = set()
+        self.function_names = set()
+        self.error_messages = []
+
+        if self.path is not None:
+            self.basename = Path(path).name
+            self.unique_name = path
+        else:
+            self.unique_name = random_string()
+        PythonSourceFile.instances[self.unique_name] = self
+        
+        # Perform AST based discovery.
+        if self.path is not None and parsing:
+            if not self.path.endswith(".py"):
+                self.error_messages.append(f"Unable to parse a non-python source file: {path}")
+                return
+            filecontent = ""
+            try:
+                filecontent = open(self.path, "r", encoding="utf8").read()
+            except OSError as err:
+                self.error_messages.append(f"Unable to read file at: {path}.\nError: {err}")
+                return
+
+            try:
+                s = ast.parse(source=filecontent, filename=self.path)
+                self.exploreTree(s)
+            except SyntaxError as err:
+                self.error_messages.append(f"Syntax error inside the file: {err}")
+
+
+    def resolveFunctionCall(self, fn_name):
+        if fn_name in self.function_names:
+            return self
+        else:
+            for i in self.import_paths:
+                resolved = self.import_paths[i].resolveFunctionCall(fn_name)
+                if resolved is not None:
+                    return resolved
+        return None
 
 if __name__ == "__main__":
     path = "toy_project/__main__.py"
-    G = nx.DiGraph()
-
-    root_ast = ast.parse(source=open(path,"r",encoding="utf8").read(), filename=path)
-
-    exploreTree(root_ast, path)
-    # Display the graph generated
+    psf = PythonSourceFile(path=path)
+    
+    G = psf.to_directed_graph()
     pos = nx.spring_layout(G, k=0.3, iterations=100)
-    nx.draw(G,pos, with_labels = True)
+
+    labeldict = {}
+    for i in PythonSourceFile.instances:
+        labeldict[i] = PythonSourceFile.instances[i].basename
+
+    nx.draw(G, pos, labels=labeldict, with_labels = True)
     plt.show()
