b957576a35bf6b3c950ae582d78fb03663a92c94
Mathïs Fédérico <mathfederico@gmail.com>
2022-08-09 10:09:48 +0200

    :memo: Refactor PythonSourceFile documentation & pylint issues

--- to_graph.py
+++ to_graph.py
@@ -128,50 +128,51 @@ class PythonFunctionDefinition:
 class PythonSourceFile:
     """
     Represents a piece of python code.
+
+    Attributes:
+        path (Optional[str]): Path to the python source file.
+        basename (str): A short and displayable name for the code, like "utils.py".
+        unique_name (Union[UniqueIdentifier, str]): A unique name that always exists.
+            If the code is in a file, it is the path. Otherwise, it can be a random string.
+        package_name (Optional[str]): If the file is part of a package,
+            the name of the package is contained here.
+        function_names (Set[str]): Set of functions that are defined by the piece of code.
+        import_paths (Set[Union[UniqueIdentifier, str]]): The unique_names of imports.
+        instances (Dict[Union[UniqueIdentifier, str], Any]): Static field containing all
+            PythonSourceFile instances.
+        error_messages (List[str]):
+
     """
 
     path: Optional[str] = None
-
-    # A short and displayable name for the code, like "utils.py"
     basename: str = ""
-
-    # A unique name that always exists. If the code is in a file, it is the path. Otherwise, it can be a random string.
     unique_name: Union[UniqueIdentifier, str]
-
-    # If the file is part of a package, the name of the package is contained here
     package_name: Optional[str] = None
-
-    # The set of functions that are defined by the piece of code
     function_names: Set[str] = set()
-
-    # The unique_names
     import_paths: Set[Union[UniqueIdentifier, str]] = set()
-
-    # Static field containing all PythonSourceFile instances
     instances: Dict[Union[UniqueIdentifier, str], Any] = {}
-
     error_messages: List[str] = []
 
-    def resolveImport(self, importName: str, exhaustiveResolution=False):
-        """
-        Resolve an import based on importName.
+    def resolve_import(self, import_name: str, exhaustive_resolution=False):
+        """Resolve an import based on import_name.
+
         Keep building the dependency tree based on it.
         This function can create new PythonSourceFile and it thus indirectly recursive.
-        importName can look like this:
-        - numpy
-        - .utils (in case of from . import utils)
-        - math
-        - math.random
-        - matplotlib.pyplot
-        All these cases are handled here.
+
+        Args:
+            import_name (str): Name of the import to resolve. Examples:
+                'numpy', 'math', 'math.random', 'matplotlib.pyplot'
+                '.utils' (in case of from . import utils)
+            exhaustive_resolution (bool): If False, ignores native dependencies.
+                Default to False.
         """
         # We don't perform import resolution if the code is not inside a file.
         if self.path is None:
             return
-        p = package_name_to_path(importName, self.path)
+        p = package_name_to_path(import_name, self.path)
 
         if p is None:
-            n = importName.split(".")
+            n = import_name.split(".")
             i = len(n)
             # Try to resolve path to packages of decreasing length.
             while p is None and i > 0:
@@ -188,123 +189,115 @@ class PythonSourceFile:
                 # But only if we're sure this was not already imported (there is an import with the same basename)
                 new_psf = None
                 for inst_id in PythonSourceFile.instances:
-                    if PythonSourceFile.instances[inst_id].basename == importName:
+                    if PythonSourceFile.instances[inst_id].basename == import_name:
                         new_psf = PythonSourceFile.instances[inst_id]
                         break
                 if new_psf is None:
                     new_psf = PythonSourceFile(None, False)
-                new_psf.basename = importName
+                new_psf.basename = import_name
                 self.import_paths.add(new_psf.unique_name)
                 return
 
-        if not exhaustiveResolution:
+        if not exhaustive_resolution:
             if "lib" in p and "site-packages" not in p:
                 # Don't resolve native dependencies to avoid noise in graph
                 # We create the PythonSourceObject, with without parsing
                 new_psf = PythonSourceFile(p, False)
-                new_psf.basename = importName
+                new_psf.basename = import_name
                 self.import_paths.add(new_psf.unique_name)
                 return
 
         new_psf = PythonSourceFile(p)
         self.import_paths.add(new_psf.unique_name)
 
-    def exploreTree(
+    def explore_tree(
         self,
         s: Union[ast.AST, Optional[ast.expr]],
         namespace: Optional[str] = "",
-        importResolution=True,
-    ):
-        """
-        Recursive depth-first explorer of the abstract syntax tree.
+        import_resolution=True,
+    ) -> None:
+        """Recursive depth-first explorer of the abstract syntax tree.
+
+        Args:
+            s (Union[ast.AST, Optional[ast.expr]]): #TODO
+            namespace (Optional[str]): #TODO
+            import_resolution (bool): #TODO
+
         """
         if s is None:
             return
         # Enumerate the types we can encounter
-        if isinstance(s, ast.Module):
-            for i in s.body:
-                self.exploreTree(i, namespace)
-        elif (
-            isinstance(s, ast.If)
-            or isinstance(s, ast.For)
-            or isinstance(s, ast.While)
-            or isinstance(s, ast.With)
-            or isinstance(s, ast.ExceptHandler)
+        if isinstance(
+            s, (ast.Module, ast.If, ast.For, ast.While, ast.With, ast.ExceptHandler)
         ):
             for i in s.body:
-                self.exploreTree(i, namespace)
+                self.explore_tree(i, namespace)
         elif isinstance(s, ast.ClassDef):
             # Ignore args, returns, type_comment and decorator_list
             if namespace is not None:
                 self.function_names.add(namespace + s.name)
             for i in s.body:
-                self.exploreTree(i, None)
+                self.explore_tree(i, None)
         elif isinstance(s, ast.FunctionDef):
             if namespace is not None:
                 namespace += s.name + "."
             for i in s.body:
-                self.exploreTree(i, namespace)
+                self.explore_tree(i, namespace)
         elif isinstance(s, ast.Try):
             for i in s.body:
-                self.exploreTree(i, None)
+                self.explore_tree(i, None)
             for j in s.handlers:
-                self.exploreTree(j, None)
+                self.explore_tree(j, None)
             for k in s.finalbody:
-                self.exploreTree(k, None)
-        elif isinstance(s, ast.Import) and importResolution:
+                self.explore_tree(k, None)
+        elif isinstance(s, ast.Import) and import_resolution:
             for name in s.names:
-                self.resolveImport(name.name)
-        elif isinstance(s, ast.ImportFrom) and importResolution:
+                self.resolve_import(name.name)
+        elif isinstance(s, ast.ImportFrom) and import_resolution:
             for name in s.names:
+                module_name = "." + name.name
                 if s.module is not None:
-                    module_name = s.module + "." + name.name
-                    self.resolveImport(module_name)
-                else:
-                    self.resolveImport("." + name.name)
+                    module_name = s.module + module_name
+                self.resolve_import(module_name)
 
-        elif isinstance(s, ast.Assign):
-            # read only the right part of the assignement
-            self.exploreTree(s.value, None)
-        elif isinstance(s, ast.AugAssign):
-            self.exploreTree(s.value, None)
-        elif isinstance(s, ast.AnnAssign):
-            self.exploreTree(s.value, None)
+        elif isinstance(s, (ast.Assign, ast.AugAssign, ast.AnnAssign)):
+            self.explore_tree(s.value, None)
         elif isinstance(s, ast.Return):
-            self.exploreTree(s.value, None)
+            self.explore_tree(s.value, None)
         elif isinstance(s, ast.Index):  # stuff inside [] when indexing array
-            self.exploreTree(s.value, None)
+            self.explore_tree(s.value, None)
         elif isinstance(s, ast.FormattedValue):  # stuff inside f-strings
-            self.exploreTree(s.value, None)
+            self.explore_tree(s.value, None)
         elif isinstance(s, ast.Expr):
-            self.exploreTree(s.value, None)
+            self.explore_tree(s.value, None)
         elif isinstance(s, ast.Subscript):
-            self.exploreTree(s.value, None)
-            self.exploreTree(s.slice, None)
+            self.explore_tree(s.value, None)
+            self.explore_tree(s.slice, None)
         elif isinstance(s, ast.BinOp):  # 3 + 5
-            self.exploreTree(s.left, None)
-            self.exploreTree(s.right, None)
+            self.explore_tree(s.left, None)
+            self.explore_tree(s.right, None)
         elif isinstance(s, ast.BoolOp):
             for vals in s.values:
-                self.exploreTree(vals, None)
+                self.explore_tree(vals, None)
         elif isinstance(s, ast.JoinedStr):
             for v in s.values:
-                self.exploreTree(v, None)
+                self.explore_tree(v, None)
         elif isinstance(s, ast.Compare):  # "s" not in stuff
-            self.exploreTree(s.left, None)
+            self.explore_tree(s.left, None)
             for v2 in s.comparators:
-                self.exploreTree(v2, None)
+                self.explore_tree(v2, None)
         elif isinstance(s, ast.Yield):
-            self.exploreTree(s.value, None)
+            self.explore_tree(s.value, None)
         elif isinstance(s, ast.Raise):
-            self.exploreTree(s.exc, None)
-        elif isinstance(s, ast.List) or isinstance(s, ast.Tuple):
+            self.explore_tree(s.exc, None)
+        elif isinstance(s, (ast.List, ast.Tuple)):
             for v3 in s.elts:
-                self.exploreTree(v3, None)
+                self.explore_tree(v3, None)
         elif isinstance(s, ast.Dict):
             for v4 in s.keys:
-                self.exploreTree(v4, None)
+                self.explore_tree(v4, None)
             for v5 in s.values:
-                self.exploreTree(v5, None)
+                self.explore_tree(v5, None)
         elif isinstance(s, ast.Constant):
             pass
         elif isinstance(s, ast.Delete):
@@ -315,7 +308,7 @@ class PythonSourceFile:
             pass  # variable outside class
         elif isinstance(s, ast.Call):  # A function call!
             for v6 in s.args:
-                self.exploreTree(v6, None)
+                self.explore_tree(v6, None)
         else:
             # Ignore these, they do not contain nested statements
             pass
@@ -374,11 +367,11 @@ class PythonSourceFile:
 
             try:
                 s = ast.parse(source=filecontent, filename=self.path)
-                self.exploreTree(s)
+                self.explore_tree(s)
             except SyntaxError as err:
                 self.error_messages.append(f"Syntax error inside the file: {err}")
 
-    def resolveFunctionCall(self, fn_name):
+    def resolve_function_call(self, fn_name):
         if fn_name in self.function_names:
             return self
         else:
