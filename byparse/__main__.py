fdf796135cd469594a8c48fb15ce3d607144c80e
Mathïs Fédérico <mathfederico@gmail.com>
2022-08-09 18:11:59 +0200

    :tada: Refactor import resolution for local packages and scripts support

--- byparse/__main__.py
+++ byparse/__main__.py
@@ -2,10 +2,14 @@ from byparse import PythonSourceFile
 
 from byparse.visualisation import to_directed_graph, networkx_to_pyvis
 
-root_path = "toy_project/__main__.py"
-root_file = PythonSourceFile(path=root_path)
 
-graph = to_directed_graph(root_file)
+if __name__ == "__main__":
+    filepath = "toy_project/package/__main__.py"
+    cwd_of_user = "toy_project"
 
-net = networkx_to_pyvis(graph, "1000px", "600px")
-net.show("nodes.html")
+    root_file = PythonSourceFile(filepath=filepath, user_cwd=cwd_of_user)
+
+    graph = to_directed_graph(root_file)
+
+    net = networkx_to_pyvis(graph, "1000px", "600px")
+    net.show("nodes.html")
--- byparse/py_src_file.py
+++ byparse/py_src_file.py
@@ -1,42 +1,93 @@
 import ast
 from typing import Optional, Union, Set, Dict, List, Any
 from pathlib import Path
+
 import importlib.util
+import importlib.machinery
 
-from byparse.identifiers import UniqueIdentifier, random_string
+from byparse.identifiers import UniqueIdentifier
 
 
-def package_name_to_path(package_name: str, filepath: str) -> Optional[str]:
+def relative_resolution(pseudo_path: str, relative_to: str) -> Optional[str]:
+    # Folder
+    package_path: Path = Path(relative_to).parent / Path(pseudo_path)
+    if package_path.exists() and package_path.is_dir():
+        return str(package_path / Path("__init__.py"))
+
+    # File
+    package_path: Path = Path(relative_to).parent / Path(pseudo_path + ".py")
+    if package_path.exists() and package_path.is_file():
+        return str(package_path)
+
+    return None
+
+
+def find_relative_path(
+    package_name: str, filepath: str, user_cwd: str
+) -> Optional[str]:
+    """Resolve a local import performed inside filepath by looking at the
+    filesystem and searching for a directory or file that the user is trying to import.
+
+    1. Try to resolve the path while ignoring the module path
+    2. If that fails, try to resolve it based on the module path
+
+    Args:
+        package_name (str): String used in the python code when the user imports
+        filepath (str): Path to the python source file where the import is performed.
+
+    Returns:
+        (str): Relative path of the file that should be imported.
+
+    Example:
+        package_name: scripts.script1 (as part of the statement "import scripts.script1 as s1")
+        filepath: ./toy_project/package/__main__.py
+        Result: ./toy_project/scripts/script1.py
+
+    """
+    pseudo_path = package_name.replace(".", "/")
+    package_path = relative_resolution(pseudo_path, filepath)
+    if package_path is not None:
+        return package_path
+
+    # In case we resolve based on the current working directory, we introduce a fake file to preserve the relative structure
+    # of the directories.
+    package_path = relative_resolution(pseudo_path, user_cwd + "/__init__.py")
+    if package_path is not None:
+        return package_path
+
+    # In case of importing function or class
+    pseudo_path = "/".join(pseudo_path.split("/")[:-1])
+    package_path = relative_resolution(pseudo_path, filepath)
+    if package_path is not None:
+        return package_path
+
+    package_path = relative_resolution(pseudo_path, user_cwd + "/__init__.py")
+    return package_path
+
+
+def package_name_to_path(
+    package_name: str, filepath: str, user_cwd: str
+) -> Optional[str]:
     """
     Return the path of the package named <package_name>
     that was imported from the file located at <filepath>
     """
-    spec = None
     try:
         spec = importlib.util.find_spec(package_name, filepath)
     except ModuleNotFoundError:
         # This occurs if an import is dependent on code, and is not supposed to be executed on this OS/Machine
         # We cannot know if imports are really executed or not as we're not solving the halting problem.
         # There is no real way to handle this case perfectly
-        pass  # Might handle this differently?
-    if spec is not None:
-        if spec.origin == "built-in":
-            return None
-        return spec.origin
-    else:
-        # Try a local resolve based on filepath:
-        package_name = package_name.replace(".", "./")
-        package_path: Path = Path(filepath).parent / Path(package_name)
-        if package_path.exists():
-            if package_path.is_dir():
-                return str(package_path / Path("__init__.py"))
-            else:
-                return str(package_path)
-        else:
-            package_path = Path(str(package_path) + ".py")
-            if package_path.exists():
-                return str(package_path)
-    return None  # Unable to resolve the package
+        # Might handle this differently?
+        spec = None
+
+    if spec is None:
+        return find_relative_path(package_name, filepath, user_cwd)
+
+    if spec.origin == "built-in" or spec.origin is None:
+        return f"built-in/{package_name}"
+
+    return spec.origin
 
 
 class PythonSourceFile:
@@ -55,11 +106,12 @@ class PythonSourceFile:
         instances (Dict[Union[UniqueIdentifier, str], Any]): Static field containing all
             PythonSourceFile instances.
         error_messages (List[str]):
-
+        user_cwd (str): The path of the console of a user that is using the project.
     """
 
-    path: Optional[str] = None
+    filepath: Optional[str] = None
     basename: str = ""
+    user_cwd: str
     unique_name: Union[UniqueIdentifier, str]
     package_name: Optional[str] = None
     function_names: Set[str] = set()
@@ -67,37 +119,37 @@ class PythonSourceFile:
     instances: Dict[Union[UniqueIdentifier, str], Any] = {}
     error_messages: List[str] = []
 
-    def __init__(self, path, parsing=True):
-        self.path = path
+    def __init__(self, filepath: str, user_cwd: str = ".", parsing: bool = True):
+        self.filepath = filepath
+        self.user_cwd = user_cwd
         self.import_paths = set()
         self.function_names = set()
         self.error_messages = []
 
-        if self.path is not None:
-            self.basename = Path(path).name
-            self.unique_name = path
-        else:
-            self.unique_name = random_string()
+        assert self.filepath is not None
+
+        self.basename = Path(filepath).name
+        self.unique_name = filepath
         PythonSourceFile.instances[self.unique_name] = self
 
         # Perform AST based discovery.
-        if self.path is not None and parsing:
-            if not self.path.endswith(".py"):
+        if self.filepath is not None and parsing:
+            if not self.filepath.endswith(".py"):
                 self.error_messages.append(
-                    f"Unable to parse a non-python source file: {path}"
+                    f"Unable to parse a non-python source file: {filepath}"
                 )
                 return
             filecontent = ""
             try:
-                filecontent = open(self.path, "r", encoding="utf8").read()
+                filecontent = open(self.filepath, "r", encoding="utf8").read()
             except OSError as err:
                 self.error_messages.append(
-                    f"Unable to read file at: {path}.\nError: {err}"
+                    f"Unable to read file at: {filepath}.\nError: {err}"
                 )
                 return
 
             try:
-                s = ast.parse(source=filecontent, filename=self.path)
+                s = ast.parse(source=filecontent, filename=self.filepath)
                 self.explore_tree(s)
             except SyntaxError as err:
                 self.error_messages.append(f"Syntax error inside the file: {err}")
@@ -115,48 +167,37 @@ class PythonSourceFile:
             exhaustive_resolution (bool): If False, ignores native dependencies.
                 Default to False.
         """
-        # We don't perform import resolution if the code is not inside a file.
-        if self.path is None:
-            return
-        p = package_name_to_path(import_name, self.path)
+        package_path = package_name_to_path(import_name, self.filepath, self.user_cwd)
 
-        if p is None:
-            n = import_name.split(".")
-            i = len(n)
-            # Try to resolve path to packages of decreasing length.
-            while p is None and i > 0:
-                p = package_name_to_path(".".join(n[0:i]), self.path)
-                i -= 1
+        new_psf = None
+        parsing = True
 
-            n.insert(0, "")
-            i = len(n)
-            while p is None and i > 1:
-                p = package_name_to_path(".".join(n[0:i]), self.path)
-                i -= 1
-            if p is None:
-                # Give up: we cannot resolve this. We still log it.
-                # But only if we're sure this was not already imported (there is an import with the same basename)
-                new_psf = None
-                for inst_id in PythonSourceFile.instances:
-                    if PythonSourceFile.instances[inst_id].basename == import_name:
-                        new_psf = PythonSourceFile.instances[inst_id]
-                        break
-                if new_psf is None:
-                    new_psf = PythonSourceFile(None, False)
-                new_psf.basename = import_name
-                self.import_paths.add(new_psf.unique_name)
-                return
+        if package_path is None:
+            # Could not resolve package path. We still log it.
+            # But only if we're sure this was not already imported
+            # (there is an import with the same basename)
+            for inst_id in PythonSourceFile.instances:
+                if PythonSourceFile.instances[inst_id].basename == import_name:
+                    new_psf = PythonSourceFile.instances[inst_id]
+                    break
 
-        if not exhaustive_resolution:
-            if "lib" in p and "site-packages" not in p:
+            if new_psf is None:
+                # raise ValueError(f"Could not resolve import {import_name}")
+                package_path = import_name
+                parsing = False
+
+        elif not exhaustive_resolution:
+            if "built-in" in package_path or (
+                "lib" in package_path and "site-packages" not in package_path
+            ):
                 # Don't resolve native dependencies to avoid noise in graph
                 # We create the PythonSourceObject, with without parsing
-                new_psf = PythonSourceFile(p, False)
-                new_psf.basename = import_name
-                self.import_paths.add(new_psf.unique_name)
-                return
+                parsing = False
 
-        new_psf = PythonSourceFile(p)
+        if new_psf is None:
+            new_psf = PythonSourceFile(package_path, self.user_cwd, parsing=parsing)
+
+        new_psf.basename = import_name
         self.import_paths.add(new_psf.unique_name)
 
     def explore_tree(
@@ -204,9 +245,9 @@ class PythonSourceFile:
                 self.resolve_import(name.name)
         elif isinstance(s, ast.ImportFrom) and import_resolution:
             for name in s.names:
-                module_name = "." + name.name
+                module_name = name.name
                 if s.module is not None:
-                    module_name = s.module + module_name
+                    module_name = ".".join((s.module, module_name))
                 self.resolve_import(module_name)
 
         elif isinstance(s, (ast.Assign, ast.AugAssign, ast.AnnAssign)):
@@ -261,8 +302,6 @@ class PythonSourceFile:
         else:
             # Ignore these, they do not contain nested statements
             pass
-            # print(ast.dump(s))
-            # print("-----")
 
     def resolve_function_call(self, fn_name):
         if fn_name in self.function_names:
