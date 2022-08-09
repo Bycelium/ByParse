68f1f1eeff369acf92c8446720edc419605ebc6c
Mathïs Fédérico <mathfederico@gmail.com>
2022-08-09 09:44:36 +0200

    :sparkles: Apply black

--- to_graph.py
+++ to_graph.py
@@ -9,35 +9,42 @@ To test this, we'll try to analyse scikit learn
 import ast
 import importlib.util
 from pathlib import Path
-import networkx as nx # type: ignore
-from pyvis.network import Network # type: ignore
-from matplotlib import pyplot as plt # type: ignore
+import networkx as nx  # type: ignore
+from pyvis.network import Network  # type: ignore
+from matplotlib import pyplot as plt  # type: ignore
 from dataclasses import dataclass
 from typing import Union, Set, List, Dict, Optional, Any
 import random
 import string
 
+
 def random_string(l=12):
     letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for _ in range(l))
+    return "".join(random.choice(letters) for _ in range(l))
+
 
 def networkx_to_pyvis(G: nx.Graph, width="1000px", height="600px"):
     pyvis_graph = Network(height, width)
-    
-    for node,node_attrs in G.nodes(data=True):
-        pyvis_graph.add_node(str(node),**node_attrs)
-        
-    for source,target,edge_attrs in G.edges(data=True):
-        if not 'value' in edge_attrs and not 'width' in edge_attrs and 'weight' in edge_attrs:
-            edge_attrs['value'] = edge_attrs['weight']
-        pyvis_graph.add_edge(str(source),str(target),**edge_attrs)
+
+    for node, node_attrs in G.nodes(data=True):
+        pyvis_graph.add_node(str(node), **node_attrs)
+
+    for source, target, edge_attrs in G.edges(data=True):
+        if (
+            not "value" in edge_attrs
+            and not "width" in edge_attrs
+            and "weight" in edge_attrs
+        ):
+            edge_attrs["value"] = edge_attrs["weight"]
+        pyvis_graph.add_edge(str(source), str(target), **edge_attrs)
 
     return pyvis_graph
 
+
 def package_name_to_path(package_name: str, filepath: str) -> Optional[str]:
     """
-        Return the path of the package named <package_name>
-        that was imported from the file located at <filepath>
+    Return the path of the package named <package_name>
+    that was imported from the file located at <filepath>
     """
     spec = None
     try:
@@ -46,24 +53,26 @@ def package_name_to_path(package_name: str, filepath: str) -> Optional[str]:
         # This occurs if an import is dependent on code, and is not supposed to be executed on this OS/Machine
         # We cannot know if imports are really executed or not as we're not solving the halting problem.
         # There is no real way to handle this case perfectly
-        pass # Might handle this differently?
+        pass  # Might handle this differently?
     if spec is not None:
-        if spec.origin == "built-in": return None
+        if spec.origin == "built-in":
+            return None
         return spec.origin
     else:
         # Try a local resolve based on filepath:
-        package_name = package_name.replace(".","./")
+        package_name = package_name.replace(".", "./")
         package_path: Path = Path(filepath).parent / Path(package_name)
         if package_path.exists():
             if package_path.is_dir():
                 return str(package_path / Path("__init__.py"))
             else:
                 return str(package_path)
-        else: 
+        else:
             package_path = Path(str(package_path) + ".py")
             if package_path.exists():
                 return str(package_path)
-    return None # Unable to resolve the package
+    return None  # Unable to resolve the package
+
 
 def test_package_name_to_path():
     """
@@ -83,14 +92,15 @@ def test_package_name_to_path():
     assert doesnotexist is None
 
 
-
 @dataclass
 class FileLocation:
     """
     Represents a specific place inside a file
     """
+
     path: str
     lint: int
+
 
 @dataclass
 class UniqueIdentifier:
@@ -98,31 +108,36 @@ class UniqueIdentifier:
     A wrapper around a string. Used to indicate that 2 strings are in the same
     context. Used more as a type hint than an actual class.
     """
+
     v: str
+
 
 @dataclass
 class PythonFunctionDefinition:
     """
     Represents a definition of a python function
     """
+
     name: str
     arguments: List[str]
-    path: str # The file where the function was defined
+    path: str  # The file where the function was defined
     line: int
     callingSites: Set[FileLocation]
+
 
 class PythonSourceFile:
     """
     Represents a piece of python code.
     """
+
     path: Optional[str] = None
 
     # A short and displayable name for the code, like "utils.py"
     basename: str = ""
-    
+
     # A unique name that always exists. If the code is in a file, it is the path. Otherwise, it can be a random string.
     unique_name: Union[UniqueIdentifier, str]
- 
+
     # If the file is part of a package, the name of the package is contained here
     package_name: Optional[str] = None
 
@@ -137,35 +152,36 @@ class PythonSourceFile:
 
     error_messages: List[str] = []
 
-    def resolveImport(self, importName: str, exhaustiveResolution = False):
+    def resolveImport(self, importName: str, exhaustiveResolution=False):
         """
-            Resolve an import based on importName.
-            Keep building the dependency tree based on it.
-            This function can create new PythonSourceFile and it thus indirectly recursive.
-            importName can look like this:
-            - numpy
-            - .utils (in case of from . import utils)
-            - math
-            - math.random
-            - matplotlib.pyplot
-            All these cases are handled here.
+        Resolve an import based on importName.
+        Keep building the dependency tree based on it.
+        This function can create new PythonSourceFile and it thus indirectly recursive.
+        importName can look like this:
+        - numpy
+        - .utils (in case of from . import utils)
+        - math
+        - math.random
+        - matplotlib.pyplot
+        All these cases are handled here.
         """
         # We don't perform import resolution if the code is not inside a file.
-        if self.path is None: return
+        if self.path is None:
+            return
         p = package_name_to_path(importName, self.path)
-        
+
         if p is None:
             n = importName.split(".")
             i = len(n)
             # Try to resolve path to packages of decreasing length.
             while p is None and i > 0:
-                p = package_name_to_path('.'.join(n[0:i]),self.path)
+                p = package_name_to_path(".".join(n[0:i]), self.path)
                 i -= 1
 
-            n.insert(0,"")
+            n.insert(0, "")
             i = len(n)
             while p is None and i > 1:
-                p = package_name_to_path('.'.join(n[0:i]),self.path)
+                p = package_name_to_path(".".join(n[0:i]), self.path)
                 i -= 1
             if p is None:
                 # Give up: we cannot resolve this. We still log it.
@@ -193,20 +209,28 @@ class PythonSourceFile:
         new_psf = PythonSourceFile(p)
         self.import_paths.add(new_psf.unique_name)
 
-    def exploreTree(self, s: Union[ast.AST,Optional[ast.expr]], namespace: Optional[str] = "", importResolution = True):
+    def exploreTree(
+        self,
+        s: Union[ast.AST, Optional[ast.expr]],
+        namespace: Optional[str] = "",
+        importResolution=True,
+    ):
         """
-            Recursive depth-first explorer of the abstract syntax tree.
+        Recursive depth-first explorer of the abstract syntax tree.
         """
-        if s is None: return
+        if s is None:
+            return
         # Enumerate the types we can encounter
         if isinstance(s, ast.Module):
             for i in s.body:
                 self.exploreTree(i, namespace)
-        elif isinstance(s, ast.If) or \
-             isinstance(s, ast.For) or \
-             isinstance(s, ast.While) or \
-             isinstance(s, ast.With) or \
-             isinstance(s, ast.ExceptHandler):
+        elif (
+            isinstance(s, ast.If)
+            or isinstance(s, ast.For)
+            or isinstance(s, ast.While)
+            or isinstance(s, ast.With)
+            or isinstance(s, ast.ExceptHandler)
+        ):
             for i in s.body:
                 self.exploreTree(i, namespace)
         elif isinstance(s, ast.ClassDef):
@@ -217,7 +241,7 @@ class PythonSourceFile:
                 self.exploreTree(i, None)
         elif isinstance(s, ast.FunctionDef):
             if namespace is not None:
-                namespace += s.name+"."
+                namespace += s.name + "."
             for i in s.body:
                 self.exploreTree(i, namespace)
         elif isinstance(s, ast.Try):
@@ -247,16 +271,16 @@ class PythonSourceFile:
             self.exploreTree(s.value, None)
         elif isinstance(s, ast.Return):
             self.exploreTree(s.value, None)
-        elif isinstance(s, ast.Index): # stuff inside [] when indexing array
+        elif isinstance(s, ast.Index):  # stuff inside [] when indexing array
             self.exploreTree(s.value, None)
-        elif isinstance(s, ast.FormattedValue): # stuff inside f-strings
+        elif isinstance(s, ast.FormattedValue):  # stuff inside f-strings
             self.exploreTree(s.value, None)
         elif isinstance(s, ast.Expr):
             self.exploreTree(s.value, None)
         elif isinstance(s, ast.Subscript):
             self.exploreTree(s.value, None)
             self.exploreTree(s.slice, None)
-        elif isinstance(s, ast.BinOp): # 3 + 5
+        elif isinstance(s, ast.BinOp):  # 3 + 5
             self.exploreTree(s.left, None)
             self.exploreTree(s.right, None)
         elif isinstance(s, ast.BoolOp):
@@ -265,7 +289,7 @@ class PythonSourceFile:
         elif isinstance(s, ast.JoinedStr):
             for v in s.values:
                 self.exploreTree(v, None)
-        elif isinstance(s, ast.Compare): # "s" not in stuff
+        elif isinstance(s, ast.Compare):  # "s" not in stuff
             self.exploreTree(s.left, None)
             for v2 in s.comparators:
                 self.exploreTree(v2, None)
@@ -286,10 +310,10 @@ class PythonSourceFile:
         elif isinstance(s, ast.Delete):
             pass
         elif isinstance(s, ast.Attribute):
-            pass # variable inside class (np.array)
+            pass  # variable inside class (np.array)
         elif isinstance(s, ast.Name):
-            pass # variable outside class
-        elif isinstance(s, ast.Call): # A function call!
+            pass  # variable outside class
+        elif isinstance(s, ast.Call):  # A function call!
             for v6 in s.args:
                 self.exploreTree(v6, None)
         else:
@@ -306,7 +330,8 @@ class PythonSourceFile:
 
         while len(visit_stack) > 0:
             node = visit_stack.pop()
-            if node in visited: continue
+            if node in visited:
+                continue
             visited.add(node)
             G.add_node(node.unique_name)
             for i in node.import_paths:
@@ -330,17 +355,21 @@ class PythonSourceFile:
         else:
             self.unique_name = random_string()
         PythonSourceFile.instances[self.unique_name] = self
-        
+
         # Perform AST based discovery.
         if self.path is not None and parsing:
             if not self.path.endswith(".py"):
-                self.error_messages.append(f"Unable to parse a non-python source file: {path}")
+                self.error_messages.append(
+                    f"Unable to parse a non-python source file: {path}"
+                )
                 return
             filecontent = ""
             try:
                 filecontent = open(self.path, "r", encoding="utf8").read()
             except OSError as err:
-                self.error_messages.append(f"Unable to read file at: {path}.\nError: {err}")
+                self.error_messages.append(
+                    f"Unable to read file at: {path}.\nError: {err}"
+                )
                 return
 
             try:
@@ -348,7 +377,6 @@ class PythonSourceFile:
                 self.exploreTree(s)
             except SyntaxError as err:
                 self.error_messages.append(f"Syntax error inside the file: {err}")
-
 
     def resolveFunctionCall(self, fn_name):
         if fn_name in self.function_names:
@@ -360,24 +388,26 @@ class PythonSourceFile:
                     return resolved
         return None
 
+
 def display_import_graph(psf: PythonSourceFile):
     """
-        Display the import graph with a matplotlib backend.
-        It's kind of ugly, so i'd recommend using the pyvis backend.
+    Display the import graph with a matplotlib backend.
+    It's kind of ugly, so i'd recommend using the pyvis backend.
     """
     G = psf.to_directed_graph()
     labeldict = {}
     for inst_id in PythonSourceFile.instances:
         labeldict[inst_id] = PythonSourceFile.instances[inst_id].basename
 
-    nx.draw(G, labels=labeldict, with_labels = True)
+    nx.draw(G, labels=labeldict, with_labels=True)
     plt.show()
+
 
 if __name__ == "__main__":
     root_path = "toy_project/__main__.py"
     root_file = PythonSourceFile(path=root_path)
-    
+
     graph = root_file.to_directed_graph()
 
-    net = networkx_to_pyvis(graph, "1000px","600px")
-    net.show('nodes.html')
+    net = networkx_to_pyvis(graph, "1000px", "600px")
+    net.show("nodes.html")
