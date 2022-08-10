71ffb8634514217b22ead57c8c0a8f8bb61e5589
Mathïs Fédérico <mathfederico@gmail.com>
2022-08-10 07:38:06 +0200

    :umbrella: Add a global variable to toy_project

--- toy_project/package/__main__.py
+++ toy_project/package/__main__.py
@@ -2,6 +2,9 @@ from package import init_package
 from package import module1
 from package import submodules
 
+GLOBAL_VAR = 2
+
+
 def main():
     print(init_package(), module1.m1(), submodules.submodule1.sm1())
 
