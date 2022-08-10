895ef9912998b7b83b6f6ebfe39ecb73cfb75987
Mathïs Fédérico <mathfederico@gmail.com>
2022-08-10 17:12:12 +0200

    :umbrella: Added some difficutlies in toy_project (as)

--- tests/integration/toy_project/package/__main__.py
+++ tests/integration/toy_project/package/__main__.py
@@ -1,12 +1,12 @@
 from package import init_package
-from package import module1
+from package import module1 as mod1
 from package import submodules
 
 GLOBAL_VAR = 2
 
 
 def main():
-    print(init_package(), module1.m1(), submodules.submodule1.sm1())
+    print(init_package(), mod1.m1(), submodules.submodule1.sm1())
 
 
 if __name__ == "__main__":
--- tests/integration/toy_project/package/submodules/submodule1.py
+++ tests/integration/toy_project/package/submodules/submodule1.py
@@ -1,4 +1,5 @@
 import scripts.script1 as s1
 
+
 def sm1():
     return f"sm1({s1.sc1()})"
