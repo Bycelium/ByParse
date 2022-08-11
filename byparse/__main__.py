72865f3771746bcc99f1d748b492da7076058e16
Mathïs Fédérico <mathfederico@gmail.com>
2022-08-11 08:55:40 +0200

    :tada: Add CLI to main to switch easily between examples

--- byparse/__main__.py
+++ byparse/__main__.py
@@ -1,11 +1,36 @@
+import argparse
+
 from byparse.visualisation import networkx_to_pyvis
 from byparse.ast_crawl import parse_project
 from byparse.graph import build_project_graph
 
-if __name__ == "__main__":
-    project_root = "tests/integration/toy_project"
+
+def cli_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--root",
+        "-r",
+        help="Root directory of the project to parse.",
+        default="tests/integration/toy_project",
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        help="Output of the html graph.",
+        default="nodes.html",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = cli_parser()
+    project_root = args.root
     module_asts = parse_project(project_root)
     graph = build_project_graph(module_asts)
     net = networkx_to_pyvis(graph)
     net.toggle_physics(True)
-    net.show("nodes.html")
+    net.show(args.output)
+
+
+if __name__ == "__main__":
+    main()
