c96de694f77aa93fb8d07914bfe5a6c266021d10
Mathïs Fédérico <mathfederico@gmail.com>
2022-08-11 06:55:34 +0200

    :umbrella: Add some example cases in toy_project

--- tests/integration/toy_project/package/module1.py
+++ tests/integration/toy_project/package/module1.py
@@ -1,5 +1,10 @@
-from package.submodules.submodule1 import sm1
+from package.submodules.submodule1 import sm1, M1
 
 
 def m1():
-    return f"m1({(sm1())})"
+    return f"m1({(sm1())}, {m11()})"
+
+
+def m11():
+    x = M1()
+    return f"m11({x.m1meth()})"
--- tests/integration/toy_project/package/module2.py
+++ tests/integration/toy_project/package/module2.py
@@ -3,3 +3,9 @@ from package.submodules.submodule2 import sm2
 
 def m2():
     return f"m2({(sm2())})"
+
+
+def m22():
+    from package.submodules.submodule1 import s1
+
+    return f"m22({s1.sbs2()})"
