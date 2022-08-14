f0e6f0cd1330871f522f81361ec99e7658d1fcd8
Mathïs Fédérico <mathfederico@gmail.com>
2022-08-14 11:32:49 +0200

    :umbrella: Add chained call import example in toy_project

--- tests/integration/toy_project/scripts/script1.py
+++ tests/integration/toy_project/scripts/script1.py
@@ -1,6 +1,6 @@
-from scripts.subscripts.subscript1 import sbs1
+import scripts.subscripts.subscript1
 from scripts.subscripts.subscript2 import sbs2
 
 
 def sc1():
-    return f"sc1({sbs1()}, {sbs2()})"
+    return f"sc1({scripts.subscripts.subscript1.sbs1()}, {sbs2()})"
