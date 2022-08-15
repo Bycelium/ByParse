9f31a1284617407a005fe2bf32ce9e70fac203c7
Mathïs Fédérico <mathfederico@gmail.com>
2022-08-15 10:18:30 +0200

    :art: Fixed missing colors in context_graph

--- byparse/visualisation/graph_vis.py
+++ byparse/visualisation/graph_vis.py
@@ -9,6 +9,7 @@ def color_context_graph(
     node_file_color: str = "rgb(55, 155, 55)",
     node_class_color: str = "#4ec994",
     node_func_color: str = "#e0e069",
+    node_lib_color: str = "grey",
     edge_path_color: str = "black",
     edge_context_color: str = "blue",
     edge_call_color: str = "#e0e069",
@@ -18,9 +19,12 @@ def color_context_graph(
         NodeType.FILE.name: node_file_color,
         NodeType.CLASS.name: node_class_color,
         NodeType.FUNCTION.name: node_func_color,
+        NodeType.LIBRAIRY.name: node_lib_color,
     }
     for n, data in graph.nodes(data=True):
-        graph.nodes[n]["color"] = node_type_color[data["type"]]
+        node_type = data["type"]
+        if node_type in node_type_color:
+            graph.nodes[n]["color"] = node_type_color[node_type]
 
     edge_type_color = {
         EdgeType.PATH.name: edge_path_color,
@@ -28,4 +32,6 @@ def color_context_graph(
         EdgeType.CALL.name: edge_call_color,
     }
     for u, v, data in graph.edges(data=True):
-        graph.edges[u, v]["color"] = edge_type_color[data["type"]]
+        edge_type = data["type"]
+        if edge_type in edge_type_color:
+            graph.edges[u, v]["color"] = edge_type_color[edge_type]
