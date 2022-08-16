e3c5c605c7990516a196994ae61e8964f6518e9f
Mathïs Fédérico <mathfederico@gmail.com>
2022-08-16 12:23:03 +0200

    :umbrella: Add unit tests for librairy import resolve

--- tests/integration/path_resolvers/test_resolve_imports.py
+++ tests/integration/path_resolvers/test_resolve_imports.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pytest
 import pytest_check as check
+from pytest_mock import MockerFixture
 
 from byparse.path_resolvers.imports import resolve_import_ast_paths
 
@@ -60,3 +61,46 @@ class TestResolveImportAstPaths:
 
         alias_to_path = resolve_import_ast_paths(import_ast, str(self.project_root))
         check.equal(alias_to_path, expected_aliases_paths)
+
+    def test_single_librairy_import(self, mocker: MockerFixture):
+        class DummySpec:
+            origin = Path("Lib", "site-packages", "matplotlib", "pyplot.py")
+
+        mocker.patch(
+            "byparse.path_resolvers.imports.find_spec",
+            return_value=DummySpec(),
+        )
+        sm_alias = ast.alias(name="matplotlib.pyplot", asname="plt")
+        import_ast = ast.Import(names=[sm_alias])
+
+        expected_aliases_paths = {sm_alias: DummySpec.origin}
+
+        alias_to_path = resolve_import_ast_paths(import_ast, str(self.project_root))
+        check.equal(set(alias_to_path.values()), set(expected_aliases_paths.values()))
+
+    def test_single_librairy_not_found(self, mocker: MockerFixture):
+
+        mocker.patch(
+            "byparse.path_resolvers.imports.find_spec",
+            return_value=None,
+        )
+        sm_alias = ast.alias(name="matplotlib.pyplot", asname="plt")
+        import_ast = ast.Import(names=[sm_alias])
+
+        expected_aliases_paths = {
+            sm_alias: Path("Lib", "site-packages", "not-found", "matplotlib.pyplot")
+        }
+
+        alias_to_path = resolve_import_ast_paths(import_ast, str(self.project_root))
+        check.equal(set(alias_to_path.values()), set(expected_aliases_paths.values()))
+
+    def test_single_librairy_buildins(self, mocker: MockerFixture):
+        sm_alias = ast.alias(name="math")
+        import_ast = ast.Import(names=[sm_alias])
+
+        expected_aliases_paths = {
+            sm_alias: Path("Lib", "site-packages", "built-in", "math")
+        }
+
+        alias_to_path = resolve_import_ast_paths(import_ast, str(self.project_root))
+        check.equal(set(alias_to_path.values()), set(expected_aliases_paths.values()))
